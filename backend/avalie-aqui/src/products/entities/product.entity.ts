bb26d2c2f384a5be8469197621a5891da3e0cd82
Paulo Victor <paulo.reis@docnix.com.br>
2023-05-19 07:25:23 -0300

    criacao classe Review

--- backend/avalie-aqui/src/products/entities/product.entity.ts
+++ backend/avalie-aqui/src/products/entities/product.entity.ts
@@ -1,7 +1,9 @@
+import { Review } from "src/reviews/entities/review.entity";
+
 export class Product {
     id: number;
     name: string;
     category: string;
     averageRating: number;
-    // review: Review;
+    review: Review[];
 }
