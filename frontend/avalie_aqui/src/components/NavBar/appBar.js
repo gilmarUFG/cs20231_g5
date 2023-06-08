d4016f4ec517059cb9a1f80f95d91f68f574b181
FilipePaco <102427696+FilipePaco@users.noreply.github.com>
2023-06-08 15:25:31 -0300

    .

--- frontend/avalie_aqui/src/components/NavBar/appBar.js
+++ frontend/avalie_aqui/src/components/NavBar/appBar.js
@@ -6,7 +6,6 @@ import decode from 'jwt-decode';
 import memories from '../../images/avalie.png';
 
 export default function Navbar() {
-
   const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -19,7 +18,7 @@ export default function Navbar() {
   };
 
   useEffect(() => {
-    const token = user?.token;
+    const token = user?.access_token;
 
     if (token) {
       const decodeToken = decode(token);
@@ -27,79 +26,68 @@ export default function Navbar() {
       if (decodeToken.exp * 1000 < new Date().getTime()) logout();
     }
 
+    
+
     setUser(JSON.parse(localStorage.getItem('profile')));
   }, [location]);
 
   return (
-    <AppBar  position="static" color="inherit">
-      <div >
-        <Typography
-         
-          variant="h2"
-          align="center"
-        >
-          Avalie Aqui!
-        </Typography>
-        <img  src={memories} alt="icon" height="60" />
-      </div>
-      <Toolbar >
-        <Button
-          component={Link}
-          to="/"
-          
-          color={location.pathname === '/' ? 'primary' : 'inherit'}
-        >
-          Home
-        </Button>
-       
-        <Button
-          component={Link}
-          to="/cadprod"
-          
-          color={
-            location.pathname === '/cadastro-produtos' ? 'primary' : 'inherit'
-          }
-        >
-          Cadastro de Produtos
-        </Button>
-        {user ? (
-          <div >
-            
-            <Button
-              variant="contained"
-              className={logout}
-              color="secondary"
-              onClick={logout}
-            >
-              Sair
-            </Button>
-          </div>
-        ) : (
+    <AppBar position="static" sx={{ backgroundColor: '#006064' }}>
+      <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        <div sx={{ display: 'flex', alignItems: 'center' }}>
+          <img src={memories} alt="icon" height="60" sx={{ marginRight: '10px' }} />
+          <Typography variant="h5" sx={{ color: 'white' }}>
+            Avalie Aqui!
+          </Typography>
+        </div>
+        <div>
           <Button
             component={Link}
-            to="/login"
-            
-            color={location.pathname === '/login' ? 'primary' : 'inherit'}
+            to="/"
+            sx={{ color: location.pathname === '/' ? 'white' : '#B2EBF2' }}
           >
-            Login
+            Home
           </Button>
-        )}
+          <Button
+            component={Link}
+            to="/cadprod"
+            sx={{ color: location.pathname === '/cadastro-produtos' ? 'white' : '#B2EBF2' }}
+          >
+            Cadastro de Produtos
+          </Button>
+          {user ? (
+            <div sx={{ display: 'flex', alignItems: 'center' }}>
+               <Avatar
+                sx={{ bgcolor: '#FF4081', marginRight: '10px' }}
+                alt={user.result.user.name}
+              >
+                {user.result.user.name.charAt(0)}
+              </Avatar>
+              <Typography variant="h6" sx={{ color: 'white' }}>
+                {user.result.user.name}
+              </Typography>
+             
+              <Button
+                variant="contained"
+                sx={{ marginLeft: '10px' }}
+                color="secondary"
+                onClick={logout}
+              >
+                Sair
+              </Button>
+            </div>
+          ) : (
+            <Button
+              component={Link}
+              to="/login"
+              sx={{ color: location.pathname === '/login' ? 'white' : '#B2EBF2' }}
+            >
+              Login
+            </Button>
+          )}
+        </div>
       </Toolbar>
     </AppBar>
   );
 }
-
-
-/*
-<Avatar
-              
-              alt={user.result.name}
-              
-            >
-              {user.result.name.charAt(0)}
-            </Avatar>
-            <Typography  variant="h6">
-              {user.name}
-            </Typography>
-
-            */
+/**/
