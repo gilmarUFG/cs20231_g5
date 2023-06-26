c560c4ae91bbf0913d27d53466c6433737e0ee7f
Henrique <henriquemartinsc@gmail.com>
2023-06-26 01:04:04 +0000

    Fix: correção validação nota de avaliação

--- backend/avalie-aqui/src/reviews/dto/create-review.dto.ts
+++ backend/avalie-aqui/src/reviews/dto/create-review.dto.ts
@@ -4,6 +4,7 @@ import {
   IsDecimal,
   IsInt,
   IsNotEmpty,
+  IsNumber,
   IsNumberString,
   IsString,
   MaxLength,
@@ -33,7 +34,7 @@ export class CreateReviewDto {
   ratedProductId: number;
 
   @IsNotEmpty()
-  @IsDecimal()
+  @IsNumber({ maxDecimalPlaces: 1 })
   @ApiProperty({
     description: 'Nota de Avaliação',
     type: 'float',
