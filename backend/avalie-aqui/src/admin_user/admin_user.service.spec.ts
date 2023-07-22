0800adc1ac1a071d1db656d30cc49cf48e80dab4
pauloviictorUFG <pvictor@discente.ufg.br>
2023-07-21 21:01:37 -0300

    correcao de classes de testes

--- backend/avalie-aqui/src/admin_user/admin_user.service.spec.ts
+++ backend/avalie-aqui/src/admin_user/admin_user.service.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AdminUserService } from './admin_user.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 describe('AdminUserService', () => {
   let service: AdminUserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AdminUserService],
+      providers: [AdminUserService, 
+        { provide: PrismaService, useValue: {} }],
     }).compile();
 
     service = module.get<AdminUserService>(AdminUserService);
